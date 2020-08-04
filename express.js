b1f0d22de5dc6ad83b1afdd9ea593657e6865f8a
Sikander Singh <sikander441@gmail.com>
2020-08-04 13:54:02 +0530

    Added swagger route

--- express.js
+++ express.js
@@ -21,7 +21,7 @@ app.use((cli, res, next) => {
     res.setHeader('Allow', "*")
     res.setHeader('Connection', "keep-alive")
     res.setHeader("Date", Date())
-    res.setHeader("Content-Type", "application/json; charset=utf-8")
+   // res.setHeader("Content-Type", "application/json; charset=utf-8")
     res.setHeader('Access-Control-Allow-Origin', "*")
     res.setHeader('Allow-Control-Allow-Methods', "*")
     res.setHeader('Access-Control-Allow-Headers', "Origin, X-Requested-With, Content-Type, Accept")
--- index.js
+++ index.js
@@ -5,8 +5,14 @@ var fs = require('fs');
 var path = require('path'); 
 var multer = require('multer'); 
 
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger');
+
+
 const app = require('./express')
 const port=process.env.PORT || 3000
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument,options));
 
 const userRouter = require('./src/routers/user')
 const articleRouter = require('./src/routers/article')
